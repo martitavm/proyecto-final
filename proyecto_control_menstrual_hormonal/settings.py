b9d5925289af62d4f06f95e333c64e5f4d49ea1a
martitavm <mvilmag028@fp.iesromerovargas.com>
2025-06-05 20:56:52 +0200

    Implementar actions y demás en el repositorio

--- proyecto_control_menstrual_hormonal/settings.py
+++ proyecto_control_menstrual_hormonal/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = '/media/'
