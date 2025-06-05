9d03478fad20aaaa93cccd0a3fca85cb848806db
martitavm <mvilmag028@fp.iesromerovargas.com>
2025-06-05 20:27:22 +0200

    Implementar actions y demás en el repositorio

--- proyecto_control_menstrual_hormonal/settings.py
+++ proyecto_control_menstrual_hormonal/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
